d7824265ab8c79ea0be484e7309971443e8ac4bd
izabayogit <izabayojonas12@gmail.com>
2019-11-25 11:10:05 +0200

    feature(userGetSpecificRed-flagAPI): user should get specific red-flag
    add API  endpoint that enables user to get a specific red-flag
    
    [finishes #169945765]

--- server/routes/entityRoutes.js
+++ server/routes/entityRoutes.js
@@ -8,6 +8,6 @@ const upload = multer({ dest: 'upload/' });
 const pathy = upload.array('files', 4);
 router.post( '/entry', Verify.userData, pathy, Entity.createEntity );
 router.get('/red-flags', Verify.userData, Entity.getall);
-
+router.get('/red-flags/:id', Verify.userData, Entity.getOne);
 
 export default router;
