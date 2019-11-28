f940a23c336ee3f986c43b575f556c0871d01426
izabayogit <izabayojonas12@gmail.com>
2019-11-29 00:54:47 +0200

    chore(updateMessagesApi): user should get mwssage after update
    update messages
    
    [finishes #170020873]

--- server/routes/entityRoutes.js
+++ server/routes/entityRoutes.js
@@ -2,15 +2,15 @@ import express from 'express';
 import multer from 'multer';
 import Entity from '../controllers/entityControllers';
 import Verify from '../middlewares/auth';
-
+import entityValidate from '../middlewares/entityValidate';
 
 const router = express.Router();
 const upload = multer({ dest: 'upload/' });
 const pathy = upload.array('files', 4);
-router.post( '/entry', Verify.userData, pathy, Entity.createEntity );
+router.post( '/entry', Verify.userData,pathy, entityValidate,  Entity.createEntity );
 router.get('/red-flags', Verify.userData, Entity.getall);
 router.get('/red-flags/:id', Verify.userData, Entity.getOne);
 router.delete('/delete/red-flags/:id', Verify.userData, Entity.delete);
-router.patch('/red-flags/:id/comment', Verify.userData, Entity.editComment);
-router.patch('/red-flags/:id/location', Verify.userData, Entity.editLocation);
+router.patch('/red-flags/:id/comment', Verify.userData, pathy,Entity.editComment);
+router.patch('/red-flags/:id/location', Verify.userData,pathy, Entity.editLocation);
 export default router;
