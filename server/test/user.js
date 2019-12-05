196e256a1f4c79310f43350a095c18b9cfd5992c
izabayogit <izabayojonas12@gmail.com>
2019-12-06 00:47:58 +0200

    feature(admin-edit-status): admin should be able to change status
    add endpoint that enables admin to change status
    
    [starts #170138920]

--- server/test/user.js
+++ server/test/user.js
@@ -326,21 +326,6 @@ describe('     ', ()=>{
                     done();
                 });
             }); 
-            it('should return error when red-flags does not exist', (done) => {
-              chai.request(server)
-                  .patch('/api/v2/admin/0')
-                  .set('Content-Type', 'application/form-data')
-                  .set('token', adminToken)
-                  .end( (err, res) => {
-                      if (err) {
-                        console.log(err)
-                      } else {
-                        expect(res.status).to.equal(404);
-                        expect(res.body.error).to.equal('red-flag with a given id was not found');
-                      }
-                      done();
-                  });
-              });
                 it('should return one red-flags', (done) => {
                   chai.request(server)
                       .delete('/api/v2/red-flags/1')
